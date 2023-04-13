ca8e0a8f56897017c324b2f9223620aec993c8ac
cleem <35063751+clemi2408@users.noreply.github.com>
2023-04-13 21:05:57 +0200

    Update ipv6.sh

--- ipv6.sh
+++ ipv6.sh
@@ -3,11 +3,11 @@ IPv6_SYSCTL_FILE=/etc/sysctl.d/10-disable-ipv6.conf
 
 ipv6_disable(){
 
-    echo "Disabling"
+    echo "INFO: Disabling IPv6 via File $IPv6_SYSCTL_FILE"
 
     if [[ ! -e $IPv6_SYSCTL_FILE ]]; then
 
-        echo "INFO: Disabling IPv6 via File $IPv6_SYSCTL_FILE"
+        echo "INFO: Creating systcl file $IPv6_SYSCTL_FILE"
 
         cat <<EOF >>$IPv6_SYSCTL_FILE
 net.ipv6.conf.all.disable_ipv6=1
@@ -17,7 +17,7 @@ EOF
 
         sysctl -p
 
-    elif [[ -f $file ]]; then
+    elif [[ -f $IPv6_SYSCTL_FILE ]]; then
 
         echo "WARN: Unable to disable IPv6 file $IPv6_SYSCTL_FILE already exists" 1>&2
 
@@ -27,11 +27,13 @@ EOF
 
 ipv6_enable(){
 
+    echo "INFO: Enabling IPv6 via File $IPv6_SYSCTL_FILE"
+
     if [[ ! -e $IPv6_SYSCTL_FILE ]]; then
 
         echo "WARN: Unable to enable IPv6 by removing file $IPv6_SYSCTL_FILE " 1>&2
 
-    elif [[ -f $file ]]; then
+    elif [[ -f $IPv6_SYSCTL_FILE ]]; then
 
         echo "INFO: Enabling IPv6 by removing file $IPv6_SYSCTL_FILE" 1>&2
         rm $IPv6_SYSCTL_FILE
