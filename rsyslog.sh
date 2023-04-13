dc56bd53312c227f41a7d5e40c27795ba36628a6
cleem <35063751+clemi2408@users.noreply.github.com>
2023-04-13 21:33:25 +0200

    Update rsyslog.sh

--- rsyslog.sh
+++ rsyslog.sh
@@ -9,7 +9,7 @@ rsyslog_enableRemote(){
     apt-get -y install rsyslog
 
     echo "INFO: Backup rsyslog config $RSYSLOG_CONFIG to $RSYSLOG_CONFIG_BACKUP"
-    cp $RSYSLOG_CONFIG $RSYSLOG_CONFIG_BACKUP
+    commons_copyFile "$RSYSLOG_CONFIG" "$RSYSLOG_CONFIG_BACKUP"
 
     echo "INFO: Enabling rsyslog tcp and udp"
     sed -i '/module(load="imudp")/s/^#//g' $RSYSLOG_CONFIG
@@ -40,7 +40,7 @@ EOF
 rsyslog_disableRemote(){
 
     echo "INFO: Restoring rsyslog config backup $RSYSLOG_CONFIG_BACKUP"
-    mv $RSYSLOG_CONFIG_BACKUP $RSYSLOG_CONFIG
+    commons_moveFile "$RSYSLOG_CONFIG_BACKUP" "$RSYSLOG_CONFIG"
 
     echo "INFO: Testing rsyslog config $RSYSLOG_CONFIG"
     rsyslogd -f $RSYSLOG_CONFIG -N1
